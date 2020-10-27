d8dfe1214d038ac0acacca43b101bad81d6aac00
Natalie Zharova <zharntl@yandex.ru>
2020-10-27 17:58:26 +0300

    refactor: client (wip)

--- client_main.c
+++ client_main.c
@@ -1,5 +1,4 @@
 
-#include <netinet/in.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <sys/types.h>
@@ -10,6 +9,37 @@
 
 #define DEFAULT_SERVER_PORT "49001"
 
+int create_client_socket(const struct addrinfo* server_addr) {
+    int client_fd = socket(server_addr->ai_family, server_addr->ai_socktype,
+                           server_addr->ai_protocol);
+    if (client_fd < 0) {
+        perror("socket() failed");
+        return -1;
+    }
+
+    int on = 1;
+    // Сделаем сокет не блокируемым.
+    if (ioctl(client_fd, FIONBIO, (char*)&on) < 0) {
+        perror("ioctl() failed");
+        close(client_fd);
+        return -1;
+    }
+
+    int connect_res;
+    do {
+        connect_res =
+            connect(client_fd, server_addr->ai_addr, server_addr->ai_addrlen);
+    } while ((connect_res < 0) &&
+             ((errno == EINPROGRESS) || (errno == EWOULDBLOCK)));
+
+    if (connect_res < 0) {
+        perror("connect() failed");
+        close(client_fd);
+        return -1;
+    }
+
+    return client_fd;
+}
 
 int main(int argc, char* argv[]) {
     char server[100];
@@ -27,42 +57,21 @@ int main(int argc, char* argv[]) {
         strcpy(port, DEFAULT_SERVER_PORT);
     }
 
-    struct addrinfo* res = get_server_address(server, port);
-    if (!res) {
+    struct addrinfo* server_addr = get_server_address(server, port);
+    if (!server_addr) {
         return 0;
     }
 
-    int client_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    int client_fd = create_client_socket(server_addr);
     if (client_fd < 0) {
-        perror("socket() failed");
-        freeaddrinfo(res);
+        freeaddrinfo(server_addr);
         return 0;
     }
 
-    do {
-        int on = 1;
-        // Сделаем сокет не блокируемым.
-        if (ioctl(client_fd, FIONBIO, (char*)&on) < 0) {
-            perror("ioctl() failed");
-            break;
-        }
-
-        int connect_res;
-        do {
-            connect_res = connect(client_fd, res->ai_addr, res->ai_addrlen);
-        } while ((connect_res < 0) &&
-                 ((errno == EINPROGRESS) || (errno == EWOULDBLOCK)));
-
-        if (connect_res < 0) {
-            perror("connect() failed");
-            break;
-        }
-
-        while (echo_client(client_fd) > 0) {
-            ;
-        }
-    } while (0);
+    while (echo_client(client_fd) > 0) {
+        ;
+    }
 
     close(client_fd);
-    freeaddrinfo(res);
+    freeaddrinfo(server_addr);
 }
