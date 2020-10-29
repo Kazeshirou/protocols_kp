b5f939a98988c1aee2c58f12081720fb4e3a1e3b
Natalie Zharova <zharntl@yandex.ru>
2020-10-29 13:13:00 +0300

    refactor: wip

--- common/end_program_handler.h
+++ common/end_program_handler.h
@@ -1,27 +1,5 @@
 #pragma once
 
-#include <stdio.h>
-#include "signal.h"
+void end_program_handler(int signum);
 
-#include "while_true.h"
-
-void end_program_handler(int signum) {
-    (void)signum;
-    printf(" Ending program by signal...\n");
-    while_true = 0;
-
-    return;
-}
-
-int set_end_program_handler() {
-    if (signal(SIGINT, end_program_handler) == SIG_ERR) {
-        perror("Can't register end program handler");
-        return -1;
-    }
-    if (signal(SIGQUIT, end_program_handler) == SIG_ERR) {
-        perror("Can't register end program handler");
-        return -1;
-    }
-
-    return 1;
-}
+int set_end_program_handler();
